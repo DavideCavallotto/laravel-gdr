67d65699670884d813c0450f88feb1d48b00cef2
Fabio <thebloodshadow14@gmail.com>
2023-12-15 16:34:29 +0100

    ADD - CharacterSeeder

---
+++ database/seeders/CharacterSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Character;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class CharacterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+
+        for ($i = 0; $i < 100; $i++) {
+            $new_character = new Character();
+
+            $new_character->nome = $faker->unique()->name();
+            $new_character->bio = $faker->paragraph(7);
+            $new_character->difesa = $faker->randomNumber(2, false) + 1;
+            $new_character->velocità = $faker->randomNumber(2, false) + 1;
+            $new_character->hp = $faker->randomNumber(2, false) + 1;
+            $new_character->save();
+        }
+    }
+}
