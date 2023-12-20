83155ad0a71c765e56276be7f229acbf69951137
Fabio <thebloodshadow14@gmail.com>
2023-12-20 17:57:35 +0100

    ADD - better style pt3

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -7,35 +7,36 @@
             <ul class="grid text-white grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 pt-16">
 
                 @forelse ($characters as $character)
-                <li>
-                <a class="bg-slate-400 p-5 flex flex-col gap-4 h-[400px] rounded-md drop-shadow-md" href="{{ route('characters.show',$character) }}">
+                    <li class=" transition-all duration-200 hover:scale-95">
+                        <a class="bg-slate-400 p-5 flex flex-col gap-4 h-[400px] rounded-md drop-shadow-md"
+                            href="{{ route('characters.show', $character) }}">
                             <h2><span class="font-bold">Nome: </span>{{ $character->nome }}</h2>
-                        <h3 class=" flex-grow overflow-auto text-base">
-                            <span class="font-bold">Bio: </span>{{ $character->bio }}
-                        </h3>
-                        <ul class="flex items-center gap-4">
-                            <li class="flex gap-2 items-center">
-                                <img class=" translate-y-[3px]" width="28" height="28"
-                                    src="https://img.icons8.com/parakeet/48/000000/shield.png" alt="shield" />
-                                <span>
-                                    = {{ $character->difesa }}
-                                </span>
-                            </li>
-                            <li class="flex gap-2 items-center">
-                                <img width="28" height="28" src="https://img.icons8.com/stencil/32/sports-mode.png"
-                                    alt="sports-mode" />
-                                <span>
-                                    = {{ $character->velocità }}
-                                </span>
-                            </li>
-                            <li class="flex gap-2 items-center">
-                                <img width="28" height="28"
-                                    src="https://img.icons8.com/office/16/undertale-heart.png" alt="undertale-heart" />
-                                <span>
-                                    = {{ $character->hp }}
-                                </span>
-                            </li>
-                        </ul>
+                            <h3 class=" flex-grow overflow-auto text-base">
+                                <span class="font-bold">Bio: </span>{{ $character->bio }}
+                            </h3>
+                            <ul class="flex items-center gap-4">
+                                <li class="flex gap-2 items-center">
+                                    <img class=" translate-y-[3px]" width="28" height="28"
+                                        src="https://img.icons8.com/parakeet/48/000000/shield.png" alt="shield" />
+                                    <span>
+                                        = {{ $character->difesa }}
+                                    </span>
+                                </li>
+                                <li class="flex gap-2 items-center">
+                                    <img width="28" height="28"
+                                        src="https://img.icons8.com/stencil/32/sports-mode.png" alt="sports-mode" />
+                                    <span>
+                                        = {{ $character->velocità }}
+                                    </span>
+                                </li>
+                                <li class="flex gap-2 items-center">
+                                    <img width="28" height="28"
+                                        src="https://img.icons8.com/office/16/undertale-heart.png" alt="undertale-heart" />
+                                    <span>
+                                        = {{ $character->hp }}
+                                    </span>
+                                </li>
+                            </ul>
                         </a>
                     </li>
                 @empty
@@ -44,6 +45,3 @@
         </div>
     </section>
 @endsection
-
-
-
